d28175d36407545c9e4b610755c1aca252d6b10d
cpolisi6526 <43207371+cpolisi6526@users.noreply.github.com>
2018-09-12 08:34:04 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    }system.out.println("My friend ate all of my leftover pasta.")
+    system.out.println("My friend ate all of my leftover pasta.");
+    }
 }
