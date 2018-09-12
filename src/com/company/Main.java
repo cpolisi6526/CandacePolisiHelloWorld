37508a5ae31b9592132d1efcb27308e354b10b5d
BT_1N3_18@bktech.local <BT_1N3_18@bktech.local>
2018-09-12 08:19:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }system.out.println("My friend ate all of my leftover pasta.")
+}
